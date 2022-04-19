abb06c455bd968cd85cb1af41b68ec0551fe5557
DivyanshPanthri <90049250+DivyanshPanthri@users.noreply.github.com>
2022-04-19 19:05:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var backImg;
+var santa, santaImg;
+var gift, gifts;
+var Divyansh, DivyanshImg;
+var Aryansh, AryanshImg;
+
+function preload() {
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
+ 
+function spawnGifts() {
+
+}
